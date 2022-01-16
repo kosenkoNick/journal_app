886999293419108e42ebf1dfa41ce4c6f9058354
nikita_kosenko <nikitonkos2002@gmail.com>
2022-01-16 15:04:43 +0200

    Implemented AttendanceService and UnitTests

--- src/main/java/com/example/journal/service/AttendanceService.java
+++ src/main/java/com/example/journal/service/AttendanceService.java
@@ -1,12 +1,13 @@
 package com.example.journal.service;
 
-import com.example.journal.model.dto.AttendanceDto;
+import com.example.journal.model.dao.AttendanceDao;
+import java.rmi.NoSuchObjectException;
 
 public interface AttendanceService {
 
-  void create(AttendanceDto dto);
-  AttendanceDto readById(Long id);
+  AttendanceDao create(AttendanceDao dao);
+  AttendanceDao readById(Long id) throws NoSuchObjectException;
 //  void update(AttendanceDto dto);
-  void delete(Long id);
+  void delete(Long id) throws NoSuchObjectException;
 
 }
