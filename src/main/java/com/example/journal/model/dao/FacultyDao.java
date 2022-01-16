160421657f61df2447a9d4ce5af9da541dac3093
nikita_kosenko <nikitonkos2002@gmail.com>
2022-01-16 16:04:28 +0200

    Implemented FacultyService and UnitTests

--- src/main/java/com/example/journal/model/dao/FacultyDao.java
+++ src/main/java/com/example/journal/model/dao/FacultyDao.java
@@ -7,14 +7,16 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
 @Getter
 @Setter
-@NoArgsConstructor
 public class FacultyDao {
 
   @Id
--- src/main/java/com/example/journal/service/impl/FacultyServiceImpl.java
+++ src/main/java/com/example/journal/service/impl/FacultyServiceImpl.java
@@ -1,21 +1,39 @@
 package com.example.journal.service.impl;
 
-import com.example.journal.model.dto.FacultyDto;
+import com.example.journal.model.dao.FacultyDao;
+import com.example.journal.repository.FacultyRepository;
 import com.example.journal.service.FacultyService;
+import java.rmi.NoSuchObjectException;
+import java.util.Optional;
+import org.springframework.stereotype.Service;
 
+@Service
 public class FacultyServiceImpl implements FacultyService {
-  @Override
-  public void create(FacultyDto dto) {
 
+  private final FacultyRepository facultyRepository;
+
+  public FacultyServiceImpl(FacultyRepository facultyRepository) {
+    this.facultyRepository = facultyRepository;
   }
 
   @Override
-  public FacultyDto readById(Long id) {
-    return null;
+  public FacultyDao create(FacultyDao dao) {
+    return facultyRepository.save(dao);
   }
 
   @Override
-  public void delete(Long id) {
+  public FacultyDao readById(Long id) throws NoSuchObjectException {
+    Optional<FacultyDao> opt = facultyRepository.findById(id);
+    if (opt.isPresent()) {
+      return opt.get();
+    } else {
+      throw new NoSuchObjectException("Problem!");
+    }
+  }
 
+  @Override
+  public void delete(Long id) throws NoSuchObjectException {
+    FacultyDao dao = readById(id);
+    facultyRepository.delete(dao);
   }
 }
