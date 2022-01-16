a2703a5f1f5d9c779280ae9c932db5a03a80d410
nikita_kosenko <nikitonkos2002@gmail.com>
2022-01-16 16:42:09 +0200

    Implemented SubjectService and UnitTests

--- src/main/java/com/example/journal/service/SubjectService.java
+++ src/main/java/com/example/journal/service/SubjectService.java
@@ -1,17 +1,16 @@
 package com.example.journal.service;
 
 import com.example.journal.model.dao.SubjectDao;
-import com.example.journal.model.dto.SubjectDto;
 import java.rmi.NoSuchObjectException;
 
 public interface SubjectService {
 
-  void create(SubjectDto dto);
+  SubjectDao create(SubjectDao dao);
 
   SubjectDao readById(Long id) throws NoSuchObjectException;
 
   //  void update(AttendanceDto dto);
 
-  void delete(Long id);
+  void delete(Long id) throws NoSuchObjectException;
 
 }
